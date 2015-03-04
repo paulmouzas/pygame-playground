bc28f99f9d90d2f4cdc147e6f8028406e0be1a24
paul <paul.mouzas@gmail.com>
2015-03-03 23:39:25 -0500

    add stars

--- spaceship.py
+++ spaceship.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 pygame.init()
 screen = pygame.display.set_mode((700, 700))
@@ -30,9 +31,12 @@ def draw_spaceship(x, y, angle):
     p2 = (math.cos(p2_angle) * magnitude + x, -math.sin(p2_angle) * magnitude + y)
     p3 = (math.cos(p3_angle) * magnitude + x, -math.sin(p3_angle) * magnitude + y)
 
-    pygame.draw.polygon(screen, white, [p1, p2, p3]) 
+    pygame.draw.line(screen, white, (p1[0], p1[1]), (p2[0], p2[1]))
+    pygame.draw.line(screen, white, (p1[0], p1[1]), (p3[0], p3[1]))
+    pygame.draw.line(screen, white, (p2[0], p2[1]), (p3[0], p3[1]))
 
-angle = math.pi
+
+angle = math.pi / 2
 angle_change = 0
 trajectory_angle = angle
 
@@ -40,6 +44,9 @@ x, y = 350, 350
 
 speed = 0
 thrust = 0
+max_speed = 8
+
+stars = [(random.randint(0, 699), random.randint(0, 699)) for x in range(140)]
 
 while running:
 
@@ -64,11 +71,17 @@ while running:
 
     screen.fill(black)
 
+    for star in stars:
+        star_x, star_y = star[0], star[1]
+        pygame.draw.line(screen, white, (star_x, star_y), (star_x, star_y))
+
     angle += angle_change
 
     trajectory_angle, speed = addVectors((trajectory_angle, speed), (angle,
         thrust))
 
+    if speed > max_speed:
+        speed = max_speed
     x += math.cos(trajectory_angle) * speed
     y -= math.sin(trajectory_angle) * speed
 
